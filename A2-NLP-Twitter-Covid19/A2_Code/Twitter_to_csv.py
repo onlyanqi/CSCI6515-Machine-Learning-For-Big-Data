adf9829f9f4a6dd47e344bf63ca80868123bf99e
Anqi Chen <caq1227@gmail.com>
2021-01-14 17:50:32 -0400

    Add files via upload

---
+++ A2-NLP-Twitter-Covid19/A2_Code/Twitter_to_csv.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 10:54:03 2020
+
+@author: RajKumar
+"""
+
+import tweepy #https://github.com/tweepy/tweepy
+import csv
+import datetime
+import re
+
+#Twitter API credentials
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+
+consumer_key='E6N3LWhA60DvI2J3aKuMGfKnu'
+consumer_secret='Pu3HIiP7ppaZY3R3hkpufToftN80iV5lHAL9mDMTlGDJmufenB'
+access_key='2960473320-xRZuI2EwvpNXXh6YfIo8aBAqIWjN71CSQw4lxqA'
+access_secret='goUZZqtLQEqFefy1Vb5OPBvDLC2RG3lrL6uTyiMh0UdfG'
+
+startDate = datetime.datetime(2019, 11, 1)
+endDate =   datetime.datetime(2020, 10, 1)
+tweets=[]
+
+
+#get tweets from the tweeter
+def get_all_tweets_for_user(screen_name):
+    
+    
+    #initialize tweepy
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    
+    #all tweets
+    alltweets = []  
+    exit=0
+    #max get 200 tweets
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+    
+    #preserve most recent tweets
+    alltweets.extend(new_tweets)
+    
+    #get last tweet id
+    oldestID = alltweets[-1].id - 1
+    
+    #get all the tweets till start date
+    while len(new_tweets) > 0 and exit==0:
+        
+        new_tweets = api.user_timeline(screen_name = screen_name,tweet_mode="extended",count=200,max_id=oldestID)
+        
+        #save recent tweets
+        alltweets.extend(new_tweets)
+        
+        #update the id of the oldest tweet less one
+        oldestID = alltweets[-1].id - 1
+        tweet_date = alltweets[-1].created_at
+        print(tweet_date)
+       # print(startDate)
+        if tweet_date < startDate:
+            exit=1
+
+    outtweets=[]
+    for tweet in alltweets:
+        if(tweet.created_at <= endDate and tweet.created_at >= startDate):
+            try:
+                text = tweet.full_text
+            except AttributeError:
+                text = tweet.text
+        
+            clean_text = re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", text)
+            outtweets.append([tweet.created_at, " "+clean_text])
+    #write the csv  
+    with open(f'{screen_name}_tweets_2019.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["created_at","text"])
+        writer.writerows(outtweets)
+    
+    pass
+
+
+if __name__ == '__main__':
+	#pass in the username of the account you want to download
+    users = ["@CanadianPM","@Safety_Canada","@CDCgov"]
+    #users = ["@CanadianPM"]
+    for user in users:
+        get_all_tweets_for_user(user)
